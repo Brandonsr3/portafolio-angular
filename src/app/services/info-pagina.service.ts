7e2184367d2bbc9cab50cb6035cb9981e17f713e
Brandon Sánchez <brandonsr1@hotmail.com>
2020-08-14 01:34:43 -0600

    Seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,19 +9,38 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
-
+  equipo: any [] = [];
   constructor(private http: HttpClient) {
 
     //  console.log('Servicio de infoPagina listo');
 
     // leer Json
-    this.http.get('assets/data/data_pagina.json')
-        .subscribe( (resp: InfoPagina) => {
-
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);
-          // console.log( resp['twitter'] );
-        });
+    this.cargarInfo();
+    this.cargarEquipo();
   }
+
+  // tslint:disable-next-line: typedef
+  private cargarInfo(){
+     // leer Json
+     this.http.get('assets/data/data_pagina.json')
+     .subscribe( (resp: InfoPagina) => {
+
+       this.cargada = true;
+       this.info = resp;
+       // console.log(resp);
+       // console.log( resp['twitter'] );
+     });
+    }
+     // tslint:disable-next-line: typedef
+     private cargarEquipo(){
+
+      this.http.get('https://angular-html-a3bf9.firebaseio.com/equipo.json')
+     .subscribe( (resp: any[]) => {
+
+       this.equipo = resp;
+       console.log(resp);
+       // console.log( resp['twitter'] );
+     });
+     }
 }
+
