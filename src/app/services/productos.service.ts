f650b89a9be62698f82e5daadb9c9b817c7ee2b3
Brandon Sánchez <brandonsr1@hotmail.com>
2021-01-22 22:26:50 -0600

    Seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
     
@@ -17,15 +18,59 @@ export class ProductosService {
   }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-a3bf9.firebaseio.com/productos_idx.json')
+
+    return new Promise<void>( (resolve, reject) => {
+
+      this.http.get('https://angular-html-a3bf9.firebaseio.com/productos_idx.json')
       .subscribe( (resp: Producto[]) => {
         this.productos = resp;
         this.cargando = false;
+        resolve();
       });
+
+    });
+
+    
   }
 
-  getProducto(id:string){
+  getProducto(id: string){
     return this.http.get(`https://angular-html-a3bf9.firebaseio.com/productos/${ id }.json`);
+
+  }
+
+  buscarProducto(termino: string){
+
+    if(this.productos.length === 0){
+      //cargar productos
+      this.cargarProductos().then( ()=>{
+        //ejecutar despues de obtener los productos
+        //aplicar filtro
+        this.filtrarProductos(termino);
+      });
+    }
+    else{
+      //aplicar el filtro
+      this.filtrarProductos(termino);
+    }
+
+    
+  }
+
+  private filtrarProductos(termino: string){
+
+    //console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach(prod =>{
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      
+      if(prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0){
+        this.productosFiltrado.push(prod);
+      }
+    });
 
   }
 }
